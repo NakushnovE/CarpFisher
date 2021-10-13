cb569dc80f43e73914d10853b3750376d2399137
NakushnovE <77289332+NakushnovE@users.noreply.github.com>
2021-10-13 21:55:14 +0400

    Add table

--- client/src/components/SessionDay.js
+++ client/src/components/SessionDay.js
@@ -1,35 +1,68 @@
 import React from 'react';
 import {Container, Table} from "react-bootstrap";
+import "../components/modals/TableCss.css"
 
 const SessionDay = () => {
     return (
         <Container>
-            <Table striped bordered hover>
+            <Table bordered hover size="sm" style={{'width': '400px'}}>
                 <thead>
                 <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody >
                 <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td className="time" style={{'width': '60px'}}>Время</td>
+                    <td style={{'width': '30px', 'height':'25px'}}>04</td>
+                    <td style={{'width': '30px', 'height':'25px'}}>10</td>
+                    <td style={{'width': '30px', 'height':'25px'}}>16</td>
+                    <td style={{'width': '30px', 'height':'25px'}}>22</td>
                 </tr>
                 <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td style={{'width': '30px', 'height':'25px'}}>ОблочностьD</td>
+                    <td>В/С</td>
+                    <td>В/С</td>
+                    <td>В/С</td>
+                    <td>В/С</td>
                 </tr>
                 <tr>
+                    <td>Осадки</td>
+                    <td>В/С</td>
+                    <td colSpan="1">В/С</td>
+                    <td>В/С </td>
+                    <td>В/С </td>
+                </tr>
+                <tr>
+                    <td>Температура</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>29</td>
+                    <td>22</td>
+                </tr>
+                <tr>
+                    <td>Давление</td>
+                    <td>763</td>
+                    <td>755</td>
+                    <td>776</td>
+                    <td>758</td>
+                </tr>
+                <tr>
+                    <td>Скорость ветра</td>
                     <td>3</td>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                    <td>5</td>
+                    <td>8</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>Направление ветра</td>
+                    <td>Ю-В</td>
+                    <td>С</td>
+                    <td>С-В</td>
+                    <td>Ю</td>
                 </tr>
                 </tbody>
             </Table>
